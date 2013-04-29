23856b51c427c345cceea1b0535fb33eee5b3dd4
Yannick Lohse <contact@yannick-lohse.fr>
2013-04-29 14:35:22 +0200

    Contenu de base

--- index.php
+++ index.php
@@ -10,16 +10,13 @@
 	
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	
-	<title></title>
+	<title>Un nouveau campus à la mesure de vos ambitions</title>
 	<meta name="description" content="">
 	<link rel="shortcut icon" href="/favicon.ico?v=0">
 	
 	<meta name="viewport" content="width=device-width">
 	
 	<link rel="stylesheet" href="/css/style.css?v=0">
-	
-	<!-- Candy -->
-	<link rel="dns-prefetch" href="//ajax.googleapis.com" />
 	
 	<!-- RSS 
 	<link rel="alternate" type="application/rss+xml" title="RSS" href="/rss.xml" />
@@ -42,12 +39,74 @@
 </head>
 <body>
 	<!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
-	<header>
-	</header>
-	<div role="main">
-	</div>
-	<footer>
-	</footer>
+	
+	<nav>
+		<ul>
+			<li>
+				<a href="#">
+					Accueil
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					Nos motivations
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					Le nouveau campus
+				</a>
+			</li>
+			<li>
+				<a href="#">
+					les travaux
+				</a>
+			</li>
+		</ul>
+	</nav>
+	
+	<section>
+		<h1>
+			Un nouveau campus
+			<span>à la mesure de vos ambitions</span>
+		</h1>
+		
+		<nav>
+			<a href="#">
+				Nos motivations
+			</a>
+			<a href="#">
+				Le nouveau campus
+			</a>
+			<a href="#">
+				Les travaux
+			</a>
+		</nav>
+	</section>
+	
+	<section>
+		<h2>
+			Pourquoi un nouveau campus ?
+		</h2>
+		<p>
+			He goes off in a huff. The whole he can endure; at the parts he baulks. Now I don't like this. I make a leg for Captain Ahab, and he wears it like a gentleman; but I make a bandbox for Queequeg, and he won't put his head into it. Are all my pains to go for nothing with that coffin? And now I'm ordered to make a life-buoy of it. It's like turning an old coat; going to bring the flesh on the other side now.
+		</p>
+		<p>
+			I don't like this cobbling sort of business—I don't like it at all; it's undignified; it's not my place. Let tinkers' brats do tinkerings; we are their betters. I like to take in hand none but clean, virgin, fair-and-square mathematical jobs, something that regularly begins at the beginning, and is at the middle when midway, and comes to an end at the conclusion; not a cobbler's job, that's at an end in the middle, and at the beginning at the end.
+		</p>
+		<p>
+			It's the old woman's tricks to be giving cobbling jobs. Lord! what an affection all old women have for tinkers. I know an old woman of sixty-five who ran away with a bald-headed young tinker once. And that's the reason I never would work for lonely widow old women ashore, when I kept my job-shop in the Vineyard; they might have taken it into their lonely old heads to run off with me. But heigh-ho! there are no caps at sea but snow-caps. Let me see.
+		</p>
+		<p>
+			 And that's the reason I never would work for lonely widow old women ashore, when I kept my job-shop in the Vineyard; they might have taken it into their lonely old heads to run off with me. But heigh-ho! there are no caps at sea but snow-caps. Let me see. Nail down the lid; caulk the seams; pay over the same with pitch; batten them down tight, and hang it with the snap-spring over the ship's stern.
+		</p>
+		<p>
+			But I'm made of knotty Aroostook hemlock; I don't budge. Cruppered with a coffin! Sailing about with a grave-yard tray! But never mind. We workers in woods make bridal-bedsteads and card-tables, as well as coffins and hearses. We work by the month, or by the job, or by the profit; not for us to ask the why and wherefore of our work, unless it be too confounded cobbling, and then we stash it if we can.
+		</p>
+	</section>
+	
+	<section>
+	</section>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="/js/libs/jquery-1.9.1.min.js"><\/script>')</script>
