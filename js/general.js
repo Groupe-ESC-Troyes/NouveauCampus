3c2bf137169cad0f63961bbc51aee47bc16a693e
Yannick Lohse <contact@yannick-lohse.fr>
2013-04-30 14:54:54 +0200

    Transitions travaux

--- js/general.js
+++ js/general.js
@@ -1,18 +1,23 @@
 $(function(){
 	$('a[rel="galerie"], a[rel="travaux"]').fancybox();
 	
-	var transitioned = false;
+	var transitionedGalerie = false,
+		transitionedTravaux = false;
 	var animationEnd = 'webkitAnimationEnd oanimationend msAnimationEnd animationend';
 	
 	$(document).on('scroll', function(){
-		if (isElementInViewport($('ul.galerie')[0]) && !transitioned){
-			transitionGalerie();
+		if (isElementInViewport($('ul.galerie')[0]) && !transitionedGalerie){
+			transitionedGalerie = true;
+			initTransition('ul.galerie li');
+		}
+		if (isElementInViewport($('ul.travaux')[0]) && !transitionedTravaux){
+			transitionedTravaux = true;
+			initTransition('ul.travaux li');
 		}
 	});
 	
-	function transitionGalerie(){
-		transitioned = true;
-		var lis = $('ul.galerie li');
+	function initTransition(selector){
+		var lis = $(selector);
 		var animated = -1;
 		
 		(function(){
